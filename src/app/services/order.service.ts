41111352e73f99ae5190f4664f20a898acfb3db9
Igor <igor.filipiuk@gmail.com>
2019-02-28 13:13:27 +0100

    accessing protected API resources

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,6 +1,7 @@
-import { Http } from '@angular/http';
+import { Http, RequestOptions, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
+import { AuthHttp } from 'angular2-jwt';
 
 @Injectable()
 export class OrderService {
@@ -8,8 +9,14 @@ export class OrderService {
   constructor(private http: Http) {
   }
 
-  getOrders() { 
-    return this.http.get('/api/orders')
+  getOrders() {
+    const headers = new Headers();
+    const token = localStorage.getItem('token');
+    headers.append('Authorization', 'Bearer' + token);
+
+    const options = new RequestOptions({ headers: headers });
+
+    return this.http.get('/api/orders', options)
       .map(response => response.json());
   }
 }
