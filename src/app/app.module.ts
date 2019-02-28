e4f2d8bedefd20bdbd2854de7c02fac785fb8aa0
Igor <igor.filipiuk@gmail.com>
2019-02-28 14:20:22 +0100

    bootstrapped home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, Http, BaseRequestOptions } from '@angular/http';
-import { RouterModule } from '@angular/router'; 
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -48,14 +48,14 @@ import { AdminAuthGuard } from './services/admin-auth-guard.service';
     AuthService,
     AuthGuard,
     AuthHttp,
-    provideAuth({
-      headerName: 'Authorization',
-      headerPrefix: 'bearer',
-      tokenName: 'token',
-      tokenGetter: (() => localStorage.getItem('token')),
-      globalHeaders: [{ 'Content-Type': 'application/json' }],
-      noJwtError: true
-    }),
+    // provideAuth({
+    //   headerName: 'Authorization',
+    //   headerPrefix: 'bearer',
+    //   tokenName: 'token',
+    //   tokenGetter: (() => localStorage.getItem('token')),
+    //   globalHeaders: [{ 'Content-Type': 'application/json' }],
+    //   noJwtError: true
+    // }),
     AdminAuthGuard,
     // For creating a mock back-end. You don't need these in a real app.
     fakeBackendProvider,
